9bd818bb00b84b02759cdfd362b297f8af242526
Tom Patterson <jemboo@gmail.com>
2020-07-02 14:59:27 -0400

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Archie.core
+[<EntryPoint>]
+let main argv =
+    //printfn "Hello World from F#!"
+    Say.hello "Yank"
+    0 // return an integer exit code
